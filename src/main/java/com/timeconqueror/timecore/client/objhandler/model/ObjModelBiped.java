d55918c8a803c504dd8e5082dcaf723f0f69855d
Andrey <timeconqueror999@gmail.com>
2019-06-30 17:57:00 +0300

    Removed changing rp in biped model

--- src/main/java/com/timeconqueror/timecore/client/objhandler/model/ObjModelBiped.java
+++ src/main/java/com/timeconqueror/timecore/client/objhandler/model/ObjModelBiped.java
@@ -113,10 +113,6 @@ public class ObjModelBiped extends ObjModelBase {
         }
 
         this.body.rotateAngleY = 0.0F;
-        this.armRight.rotationPointZ = 0.0F;
-        this.armRight.rotationPointX = -5.0F;
-        this.armLeft.rotationPointZ = 0.0F;
-        this.armLeft.rotationPointX = 5.0F;
         float f = 1.0F;
 
         if (flag) {
@@ -153,52 +149,6 @@ public class ObjModelBiped extends ObjModelBase {
 
         this.armRight.rotateAngleY = 0.0F;
         this.armRight.rotateAngleZ = 0.0F;
-
-        if (this.swingProgress > 0.0F) {
-            EnumHandSide enumhandside = this.getMainHand(entityIn);
-            ObjModelRenderer modelrenderer = this.getArmForSide(enumhandside);
-            float f1 = this.swingProgress;
-            this.body.rotateAngleY = MathHelper.sin(MathHelper.sqrt(f1) * ((float) Math.PI * 2F)) * 0.2F;
-
-            if (enumhandside == EnumHandSide.LEFT) {
-                this.body.rotateAngleY *= -1.0F;
-            }
-
-            this.armRight.rotationPointZ = MathHelper.sin(this.body.rotateAngleY) * 5.0F;
-            this.armRight.rotationPointX = -MathHelper.cos(this.body.rotateAngleY) * 5.0F;
-            this.armLeft.rotationPointZ = -MathHelper.sin(this.body.rotateAngleY) * 5.0F;
-            this.armLeft.rotationPointX = MathHelper.cos(this.body.rotateAngleY) * 5.0F;
-            this.armRight.rotateAngleY += this.body.rotateAngleY;
-            this.armLeft.rotateAngleY += this.body.rotateAngleY;
-            this.armLeft.rotateAngleX += this.body.rotateAngleY;
-            f1 = 1.0F - this.swingProgress;
-            f1 = f1 * f1;
-            f1 = f1 * f1;
-            f1 = 1.0F - f1;
-            float f2 = MathHelper.sin(f1 * (float) Math.PI);
-            float f3 = MathHelper.sin(this.swingProgress * (float) Math.PI) * -(this.head.rotateAngleX - 0.7F) * 0.75F;
-            modelrenderer.rotateAngleX = (float) ((double) modelrenderer.rotateAngleX - ((double) f2 * 1.2D + (double) f3));
-            modelrenderer.rotateAngleY += this.body.rotateAngleY * 2.0F;
-            modelrenderer.rotateAngleZ += MathHelper.sin(this.swingProgress * (float) Math.PI) * -0.4F;
-        }
-
-        if (this.isSneak) {
-            this.body.rotateAngleX = 0.5F;
-            this.armRight.rotateAngleX += 0.4F;
-            this.armLeft.rotateAngleX += 0.4F;
-            this.legRight.rotationPointZ = 4.0F;
-            this.legLeft.rotationPointZ = 4.0F;
-            this.legRight.rotationPointY = 9.0F;
-            this.legLeft.rotationPointY = 9.0F;
-            this.head.rotationPointY = 1.0F;
-        } else {
-            this.body.rotateAngleX = 0.0F;
-            this.legRight.rotationPointZ = 0.1F;
-            this.legLeft.rotationPointZ = 0.1F;
-            this.legRight.rotationPointY = 12.0F;
-            this.legLeft.rotationPointY = 12.0F;
-            this.head.rotationPointY = 0.0F;
-        }
 
         this.armRight.rotateAngleZ += MathHelper.cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
         this.armLeft.rotateAngleZ -= MathHelper.cos(ageInTicks * 0.09F) * 0.05F + 0.05F;
