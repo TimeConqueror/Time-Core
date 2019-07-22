8211635550093831b4751b17b328ef507424e2d7
Andrey <timeconqueror999@gmail.com>
2019-07-22 13:18:47 +0300

    Fixed bug with obj model rendering

--- src/main/java/ru/timeconqueror/timecore/client/obj/loader/part/Face.java
+++ src/main/java/ru/timeconqueror/timecore/client/obj/loader/part/Face.java
@@ -51,8 +51,7 @@ public class Face {
         Vec3d v1 = new Vec3d(vertices[1].x - vertices[0].x, vertices[1].y - vertices[0].y, vertices[1].z - vertices[0].z);
         Vec3d v2 = new Vec3d(vertices[2].x - vertices[0].x, vertices[2].y - vertices[0].y, vertices[2].z - vertices[0].z);
 
-        Vec3d normalVector = v1.crossProduct(v2);
-        normalVector.normalize();
+        Vec3d normalVector = v1.crossProduct(v2).normalize();
 
         return new Vertex((float) normalVector.x, (float) normalVector.y, (float) normalVector.z);
     }
