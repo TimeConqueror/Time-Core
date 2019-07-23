d8e24329176b9771f07dab1ec3e1e939b24f2638
Andrey <timeconqueror999@gmail.com>
2019-07-23 13:56:09 +0300

    Added link to wiki page

--- src/main/java/ru/timeconqueror/timecore/api/client/obj/ObjModelLoader.java
+++ src/main/java/ru/timeconqueror/timecore/api/client/obj/ObjModelLoader.java
@@ -12,10 +12,12 @@ public class ObjModelLoader {
      * <p>
      * If you want to rotate some parts of your model, you'll need to set rotation points.
      * This can be done by creating .rp file which name is equal to obj file name (you can see an example in "resources" package)
-     * The instruction that explains how to create .rp file you can see in wiki page on github (//TODO add link).
+     * The instruction that explains how to create .rp file you can see in wiki page on github (look at @see)
      *
      * @param resourceLocation resourceLocation of obj file.
      * @return built Obj Model which you can use in Entity Models or TESRs.
+     *
+     * @see <a href="https://github.com/TimeConqueror/Time-Core/wiki/.rp-File-Creation">https://github.com/TimeConqueror/Time-Core/wiki/.rp-File-Creation</a>
      */
     public static AbstractObjModel load(ResourceLocation resourceLocation) {
         return new ObjModelBuilder(resourceLocation).loadModel();
