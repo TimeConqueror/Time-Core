9ca9e01cf51ab78a04f3e1deedaa9b8b2cbcc076
TimeConqueror <timeconqueror999@gmail.com>
2020-02-16 19:01:38 +0300

    Fixed and optimized TimeMod#setupAutoRegistries Updated forge

--- src/main/java/ru/timeconqueror/timecore/api/TimeMod.java
+++ src/main/java/ru/timeconqueror/timecore/api/TimeMod.java
@@ -2,46 +2,59 @@ package ru.timeconqueror.timecore.api;
 
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
 import net.minecraftforge.forgespi.language.ModFileScanData;
+import ru.timeconqueror.timecore.TimeCore;
 import ru.timeconqueror.timecore.api.common.event.FMLModConstructedEvent;
 import ru.timeconqueror.timecore.api.common.registry.ForgeTimeRegistry;
 import ru.timeconqueror.timecore.api.common.registry.TimeAutoRegistry;
 
+/**
+ * Base mod class needed for TimeCore features.
+ * <p>
+ * Your main mod class should extend this if you want to apply such features as auto-registry.
+ */
 public abstract class TimeMod {
     public TimeMod() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onModConstructed);
     }
 
-    private static void setupAutoRegistries() {
-        ModList.get().getMods().forEach(modInfo -> {
-            ModFileScanData scanData = modInfo.getOwningFile().getFile().getScanResult();
+    private void setupAutoRegistries() {
+        for (ModInfo modInfo : ModList.get().getMods()) {
+            if (modInfo.getModId().equals(getModID())) {
+                ModFileScanData scanData = modInfo.getOwningFile().getFile().getScanResult();
 
-            scanData.getAnnotations().stream()
-                    .filter(annotationData -> annotationData.getAnnotationType().equals(TimeAutoRegistry.ASM_TYPE))
-                    .forEach(annotationData -> {
-                        Class<?> regClass = null;
-                        try {
-                            regClass = Class.forName(annotationData.getClassType().getClassName());
+                scanData.getAnnotations().stream()
+                        .filter(annotationData -> annotationData.getAnnotationType().equals(TimeAutoRegistry.ASM_TYPE))
+                        .forEach(annotationData -> {
+                            Class<?> regClass = null;
+                            try {
+                                regClass = Class.forName(annotationData.getClassType().getClassName());
 
-                            Object obj = regClass.newInstance();
+                                Object obj = regClass.newInstance();
 
-                            if (obj instanceof ForgeTimeRegistry<?>) {
-                                FMLJavaModLoadingContext.get().getModEventBus().register(obj);
-                            } else
-                                throw new RuntimeException("Annotated class with AutoRegistry annotation " + obj.getClass() + " doesn't extend " + ForgeTimeRegistry.class.getSimpleName());
+                                if (obj instanceof ForgeTimeRegistry<?>) {
+                                    FMLJavaModLoadingContext.get().getModEventBus().register(obj);
+                                } else
+                                    throw new RuntimeException("Annotated class with AutoRegistry annotation " + obj.getClass() + " doesn't extend " + ForgeTimeRegistry.class.getSimpleName());
 
-                        } catch (ReflectiveOperationException e) {
-                            if (e.getCause() instanceof NoSuchMethodException) {
-                                throw new RuntimeException("TimeCore AutoRegistry can't find constructor with no parameters for " + regClass, e);
-                            } else {
-                                throw new RuntimeException(e);
+                            } catch (ReflectiveOperationException e) {
+                                if (e.getCause() instanceof NoSuchMethodException) {
+                                    throw new RuntimeException("TimeCore AutoRegistry can't find constructor with no parameters for " + regClass, e);
+                                } else {
+                                    throw new RuntimeException(e);
+                                }
                             }
-                        }
-                    });
-        });
+                        });
+
+                TimeCore.LOGGER.debug("Loaded Auto-Registries for {} ({})", modInfo.getDisplayName(), modInfo.getModId());
+
+                break;
+            }
+        }
     }
 
-    public void onModConstructed(FMLModConstructedEvent event) {
+    private void onModConstructed(FMLModConstructedEvent event) {
         setupAutoRegistries();
     }
 
