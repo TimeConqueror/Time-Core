f17a9c8190ce2eefc3eaae405c5f136622107aa4
TimeConqueror <timeconqueror999@gmail.com>
2020-06-30 18:30:37 +0300

    Fixed some bugs

--- src/main/java/ru/timeconqueror/timecore/client/render/model/TimeEntityModel.java
+++ src/main/java/ru/timeconqueror/timecore/client/render/model/TimeEntityModel.java
@@ -13,10 +13,30 @@ public class TimeEntityModel<T extends Entity> extends EntityModel<T> {
         model.boxList.clear();
     }
 
+    /**
+     * Sets custom scale for the model.
+     * <p>
+     * Should only be called once and before first render frame,
+     * otherwise you'll see unexpected render behaviour.
+     */
+    public TimeEntityModel<T> setScaleMultiplier(float scaleMultiplier) {
+        model.setScaleMultiplier(scaleMultiplier);
+
+        return this;
+    }
+
+    /**
+     * Renders model with provided scale.
+     *
+     * @param initialScale controls initial scale settings of the model.
+     *                     Once you provided some number as initial scale,
+     *                     you should always provide this particular number,
+     *                     otherwise you'll see unexpected render behaviour.
+     */
     @Override
-    public void render(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+    public void render(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float initialScale) {
         GlStateManager.translatef(0, 1.501F, 0);//Mojang, WHY???
-        model.render(scale);
+        model.render(initialScale);
         GlStateManager.translatef(0, -1.501F, 0);
     }
 
