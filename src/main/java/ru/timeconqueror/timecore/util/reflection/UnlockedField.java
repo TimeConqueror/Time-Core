ff19528f76b9d5e4aa74ce691350af614c385411
TimeConqueror <timeconqueror999@gmail.com>
2020-09-15 01:25:26 +0300

    Quick anno reference change

--- src/main/java/ru/timeconqueror/timecore/util/reflection/UnlockedField.java
+++ src/main/java/ru/timeconqueror/timecore/util/reflection/UnlockedField.java
@@ -1,6 +1,7 @@
 package ru.timeconqueror.timecore.util.reflection;
 
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
