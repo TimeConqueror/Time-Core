621d1a1886f201a206309af95899e17e32eec7c7
TimeConqueror <timeconqueror999@gmail.com>
2020-12-09 04:04:58 +0300

    Added ability to turn off generation noise smoothing for marked pieces

---
+++ src/main/java/ru/timeconqueror/timecore/api/common/world/structure/INoNoiseStructurePiece.java
@@ -0,0 +1,7 @@
+package ru.timeconqueror.timecore.api.common.world.structure;
+
+/**
+ * Noise smoother won't apply on structure pieces, which implement this interface.
+ */
+public interface INoNoiseStructurePiece {
+}
