3a9afba2950fa1677a8943cfb90c7b0f8732da98
TimeConqueror <timeconqueror999@gmail.com>
2020-12-09 04:07:06 +0300

    Added more javadoc to INoNoiseStructurePiece

--- src/main/java/ru/timeconqueror/timecore/api/common/world/structure/INoNoiseStructurePiece.java
+++ src/main/java/ru/timeconqueror/timecore/api/common/world/structure/INoNoiseStructurePiece.java
@@ -1,7 +1,10 @@
 package ru.timeconqueror.timecore.api.common.world.structure;
 
+import ru.timeconqueror.timecore.registry.newreg.StructureRegister.StructureRegisterChain;
+
 /**
  * Noise smoother won't apply on structure pieces, which implement this interface.
+ * Only works with structures with enabled {@link StructureRegisterChain#transformsSurroundingLand()}
  */
 public interface INoNoiseStructurePiece {
 }
