0c6634a9d7ae028d9fa00815e8796606ad888857
TimeConqueror <timeconqueror999@gmail.com>
2020-12-23 15:29:49 +0300

    Fixed Promised, added ConfiguredFeature Register

--- src/main/java/ru/timeconqueror/timecore/registry/InsertablePromised.java
+++ src/main/java/ru/timeconqueror/timecore/registry/InsertablePromised.java
@@ -1,28 +1,13 @@
 package ru.timeconqueror.timecore.registry;
 
 import net.minecraft.util.ResourceLocation;
-import org.jetbrains.annotations.Nullable;
 
-public class InsertablePromised<T> implements Promised<T> {
-    private final ResourceLocation id;
-    @Nullable
-    private T value = null;
-
+public class InsertablePromised<T> extends Promised<T> {
     public InsertablePromised(ResourceLocation id) {
-        this.id = id;
-    }
-
-    @Override
-    public T get() {
-        return null;
+        super(id);
     }
 
     public void insert(T value) {
         this.value = value;
-    }
-
-    @Override
-    public ResourceLocation getId() {
-        return id;
     }
 }
---
+++ src/main/java/ru/timeconqueror/timecore/registry/newreg/ConfiguredFeatureRegister.java
@@ -0,0 +1,78 @@
+package ru.timeconqueror.timecore.registry.newreg;
+
+import net.minecraft.util.registry.WorldGenRegistries;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import ru.timeconqueror.timecore.registry.Promised;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+/**
+ * This is the register for {@link ConfiguredFeature}.
+ * If you need to register features, use {@link SimpleForgeRegister}
+ *///TODO [minecraft/WorldSettingsImport]: Error adding element: Spread too big: 6 > 4
+public class ConfiguredFeatureRegister extends VanillaRegister<ConfiguredFeature<?, ?>> {
+    private final List<FeatureInfo> featureInfoList = new ArrayList<>();
+
+    public ConfiguredFeatureRegister(String modId) {
+        super(modId, WorldGenRegistries.CONFIGURED_FEATURE);
+    }
+
+    public <I extends ConfiguredFeature<?, ?>> ConfiguredFeatureRegistryChain<I> register(String name, GenerationStage.Decoration genStage, Supplier<I> configuredFeatureSup) {
+        Promised<I> promised = registerEntry(name, configuredFeatureSup);
+
+        return new ConfiguredFeatureRegistryChain<>(promised, genStage);
+    }
+
+    @Override
+    public void regToBus(IEventBus bus) {
+        super.regToBus(bus);
+        MinecraftForge.EVENT_BUS.addListener(this::onBiomeLoad);
+    }
+
+    private void onBiomeLoad(BiomeLoadingEvent event) {
+        for (FeatureInfo info : featureInfoList) {
+            if (info.biomePredicate.test(event)) {
+                event.getGeneration().getFeatures(info.stage).add(info.promisedFeature::get);
+            }
+        }
+    }
+
+    public class ConfiguredFeatureRegistryChain<I extends ConfiguredFeature<?, ?>> extends RegisterChain<I> {
+        private final FeatureInfo featureInfo;
+
+        private ConfiguredFeatureRegistryChain(Promised<I> promised, GenerationStage.Decoration stage) {
+            super(promised);
+
+            featureInfo = new FeatureInfo(promised, stage);
+            featureInfoList.add(featureInfo);
+        }
+
+        /**
+         * Controls, if feature can be generated in provided biome.
+         * By default it will be registered for all biomes.
+         */
+        public ConfiguredFeatureRegistryChain<I> setBiomePredicate(Predicate<BiomeLoadingEvent> biomePredicate) {
+            this.featureInfo.biomePredicate = biomePredicate;
+            return this;
+        }
+    }
+
+    private static class FeatureInfo {
+        private final Promised<? extends ConfiguredFeature<?, ?>> promisedFeature;
+        private final GenerationStage.Decoration stage;
+
+        public FeatureInfo(Promised<? extends ConfiguredFeature<?, ?>> promisedFeature, GenerationStage.Decoration stage) {
+            this.promisedFeature = promisedFeature;
+            this.stage = stage;
+        }
+
+        private Predicate<BiomeLoadingEvent> biomePredicate = event -> true;
+    }
+}
