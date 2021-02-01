5b0e8993be281d0651972bfea08e187dfe5981cb
TimeConqueror <timeconqueror999@gmail.com>
2021-02-01 15:12:57 +0300

    Fixed test class crash on server

--- src/test/java/ru/timeconqueror/timecore/animation_example/entity_example/client/render/RenderFloro.java
+++ src/test/java/ru/timeconqueror/timecore/animation_example/entity_example/client/render/RenderFloro.java
@@ -4,13 +4,13 @@ import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
 import ru.timeconqueror.timecore.TimeCore;
 import ru.timeconqueror.timecore.animation.renderer.AnimatedLivingEntityRenderer;
+import ru.timeconqueror.timecore.animation_example.entity_example.client.Models;
 import ru.timeconqueror.timecore.animation_example.entity_example.entity.FloroEntity;
-import ru.timeconqueror.timecore.animation_example.entity_example.registry.EntityRegistry;
 import ru.timeconqueror.timecore.client.render.model.TimeEntityModel;
 
 public class RenderFloro extends AnimatedLivingEntityRenderer<FloroEntity, TimeEntityModel<FloroEntity>> {
     public RenderFloro(EntityRendererManager rendererManager) {
-        super(rendererManager, EntityRegistry.floroModel.setScaleMultiplier(1.6F), 0.5F);
+        super(rendererManager, Models.floroModel.setScaleMultiplier(1.6F), 0.5F);
     }
 
     @Override
