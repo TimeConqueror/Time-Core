f5e58d56aeddf9bf05e98300fbdddefe46bf86d0
TimeConqueror <timeconqueror999@gmail.com>
2021-04-04 16:00:22 +0300

    DrawHelper: added color interpolation method

--- src/main/java/ru/timeconqueror/timecore/api/util/client/DrawHelper.java
+++ src/main/java/ru/timeconqueror/timecore/api/util/client/DrawHelper.java
@@ -322,6 +322,40 @@ public class DrawHelper {
     }
 
     /**
+     * Interpolates the color between a and b.
+     *
+     * @param a       the first argb color
+     * @param b       the second argb color
+     * @param percent percentage, from 0 to 1
+     */
+    public static int interpolateColor(int a, int b, float percent) {
+        if (percent <= 0) {
+            return a;
+        } else if (percent >= 1) {
+            return b;
+        }
+
+        int ra = (a >> 16) & 0xFF;
+        int ga = (a >> 8) & 0xFF;
+        int ba = a & 0xFF;
+        int aa = a >> 24;
+
+        int rb = (b >> 16) & 0xFF;
+        int gb = (b >> 8) & 0xFF;
+        int bb = b & 0xFF;
+        int ab = b >> 24;
+
+        float p1 = 1 - percent;
+
+        int rc = (int) (p1 * ra + percent * rb);
+        int gc = (int) (p1 * ga + percent * gb);
+        int bc = (int) (p1 * ba + percent * bb);
+        int ac = (int) (p1 * aa + percent * ab);
+
+        return (ac << 24) + (rc << 16) + (gc << 8) + bc;
+    }
+
+    /**
      * Draws string.
      *
      * @param text  text to be displayed.
