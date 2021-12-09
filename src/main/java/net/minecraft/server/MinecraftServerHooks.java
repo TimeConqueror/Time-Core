d4b57d8607e9b9273054680cab46ef3ba9adf46d
TimeConqueror <timeconqueror999@gmail.com>
2021-12-09 06:21:48 +0300

    Upd to 36.2

--- src/main/java/net/minecraft/server/MinecraftServerHooks.java
+++ src/main/java/net/minecraft/server/MinecraftServerHooks.java
@@ -1,10 +1,9 @@
 package net.minecraft.server;
 
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.storage.SaveFormat;
 
 public class MinecraftServerHooks {
-    public static SaveFormat.LevelSave getStorageSource(MinecraftServer server){
+    public static SaveFormat.LevelSave getStorageSource(MinecraftServer server) {
         return server.storageSource;
     }
 }
