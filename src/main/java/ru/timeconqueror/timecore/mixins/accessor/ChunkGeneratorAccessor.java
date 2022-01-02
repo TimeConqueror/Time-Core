3bd9e939de291a0ae5b02da5c291cbb298f02940
TimeConqueror <timeconqueror999@gmail.com>
2022-01-02 14:46:32 +0300

    Ported StructureFeatureRegister

---
+++ src/main/java/ru/timeconqueror/timecore/mixins/accessor/ChunkGeneratorAccessor.java
@@ -0,0 +1,12 @@
+package ru.timeconqueror.timecore.mixins.accessor;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(ChunkGenerator.class)
+public interface ChunkGeneratorAccessor {
+    @Invoker("codec")
+    Codec<? extends ChunkGenerator> codec();
+}
