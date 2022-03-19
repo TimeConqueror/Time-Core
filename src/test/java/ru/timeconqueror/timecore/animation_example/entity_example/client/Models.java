32ef6efbee8313a202333a7e9fd4b79be601622c
TimeConqueror <timeconqueror999@gmail.com>
2022-03-19 19:13:57 +0300

    Removed redundant event listener registration

--- src/test/java/ru/timeconqueror/timecore/animation_example/entity_example/client/Models.java
+++ src/test/java/ru/timeconqueror/timecore/animation_example/entity_example/client/Models.java
@@ -1,13 +1,10 @@
 package ru.timeconqueror.timecore.animation_example.entity_example.client;
 
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.common.Mod;
 import ru.timeconqueror.timecore.TimeCore;
 import ru.timeconqueror.timecore.api.registry.TimeModelRegister;
 import ru.timeconqueror.timecore.api.registry.util.AutoRegistrable;
 import ru.timeconqueror.timecore.client.render.model.TimeModelLocation;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class Models {
     @AutoRegistrable
     private static final TimeModelRegister REGISTER = new TimeModelRegister(TimeCore.MODID);
