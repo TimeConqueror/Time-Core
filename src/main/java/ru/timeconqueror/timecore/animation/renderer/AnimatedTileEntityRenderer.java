641454bbb5c3a3628b0ac2c88c7486db94bc38a8
TimeConqueror <timeconqueror999@gmail.com>
2022-03-19 19:03:49 +0300

    Fixed tile renderer not resetting model

--- src/main/java/ru/timeconqueror/timecore/animation/renderer/AnimatedTileEntityRenderer.java
+++ src/main/java/ru/timeconqueror/timecore/animation/renderer/AnimatedTileEntityRenderer.java
@@ -19,6 +19,8 @@ public abstract class AnimatedTileEntityRenderer<T extends BlockEntity & Animate
 
     @Override
     public void render(T tileEntityIn, float partialTicks, PoseStack matrixStackIn, MultiBufferSource bufferIn, int combinedLightIn, int combinedOverlayIn) {
+        getModel().reset();
+
         tileEntityIn.getSystem().getAnimationManager().applyAnimations(getModel());
 
         ResourceLocation texture = getTexture(tileEntityIn);
