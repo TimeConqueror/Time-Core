449176f539b29f58376128c8197ecf4eb9f1807f
TimeConqueror <timeconqueror999@gmail.com>
2022-06-30 21:18:50 +0300

    Ported Experimental Warning Suppressing

--- src/main/java/ru/timeconqueror/timecore/mixins/client/MinecraftMixin.java
+++ src/main/java/ru/timeconqueror/timecore/mixins/client/MinecraftMixin.java
@@ -2,29 +2,31 @@ package ru.timeconqueror.timecore.mixins.client;
 
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import ru.timeconqueror.timecore.TimeCore;
+import ru.timeconqueror.timecore.internal.common.config.MainConfig;
 
 @Mixin(Minecraft.class)//TODO check if it works in default runtime
 public abstract class MinecraftMixin {
-    //FIXME
-    //  @SuppressWarnings("InvalidInjectorMethodSignature")
-//    @ModifyVariable(method = "doLoadLevel",
-//            at = @At(
-//                    value = "STORE",
-//                    ordinal = 0
-//            ),
-//            index = 13, // setting the flag1
-//            remap = false)
-//    private boolean suppressExperimentalWarning(boolean containsExperimentalFeatures) {
-//        if (containsExperimentalFeatures) {
-//            TimeCore.LOGGER.info("The world contains experimental features.");
-//
-//            if (MainConfig.INSTANCE.suppressExperimentalWarning.get()) {
-//                TimeCore.LOGGER.info("Suppressing warning about using experimental features...");
-//
-//                return false;
-//            }
-//        }
-//
-//        return containsExperimentalFeatures;
-    // }
+    @SuppressWarnings("InvalidInjectorMethodSignature")
+    @ModifyVariable(method = "doLoadLevel",
+            at = @At(
+                    value = "STORE"
+            ),
+            index = 12, // setting the flag1
+            remap = false)
+    private boolean suppressExperimentalWarning(boolean containsExperimentalFeatures) {
+        if (containsExperimentalFeatures) {
+            TimeCore.LOGGER.info("The world contains experimental features.");
+
+            if (MainConfig.INSTANCE.suppressExperimentalWarning.get()) {
+                TimeCore.LOGGER.info("Suppressing warning about using experimental features...");
+
+                return false;
+            }
+        }
+
+        return containsExperimentalFeatures;
+    }
 }
