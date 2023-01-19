eb470bdae193b14e125cca99745b652c7f33a087
TimeConqueror <timeconqueror999@gmail.com>
2023-01-19 20:58:46 +0300

    Add player coffee capability example | cherry-pick 1.19.3

---
+++ src/test/java/ru/timeconqueror/timecore/capability_example/player/CapabilityTest.java
@@ -0,0 +1,21 @@
+package ru.timeconqueror.timecore.capability_example.player;
+
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class CapabilityTest {
+
+    @SubscribeEvent
+    public static void onTick(TickEvent.PlayerTickEvent event) {
+        if (event.side.isServer() && event.phase == TickEvent.Phase.END) {
+            MyPlayerCapability cap = MyPlayerCapability.of(event.player);
+
+            if (cap != null) {
+                cap.ticks.set(cap.ticks.get() + 1);
+                // System.out.println("Cap Ticks: " + cap.ticks.get());
+            }
+        }
+    }
+}
