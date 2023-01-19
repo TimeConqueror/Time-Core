8456042b73516cb6462adfb3e6f411d9c46d5bc8
TimeConqueror <timeconqueror999@gmail.com>
2023-01-19 20:58:46 +0300

    Add player coffee capability example

---
+++ src/test/java/ru/timeconqueror/timecore/capability_example/player/CEPCaps.java
@@ -0,0 +1,24 @@
+package ru.timeconqueror.timecore.capability_example.player;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import ru.timeconqueror.timecore.TimeCore;
+import ru.timeconqueror.timecore.api.registry.CapabilityRegister;
+import ru.timeconqueror.timecore.api.registry.util.AutoRegistrable;
+import ru.timeconqueror.timecore.common.capability.owner.CapabilityOwner;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class CEPCaps {
+    @AutoRegistrable
+    private static final CapabilityRegister REGISTER = new CapabilityRegister(TimeCore.MODID);
+
+    public static final Capability<MyPlayerCapability> MY_CAPABILITY = REGISTER.register(MyPlayerCapability.class);
+
+    @SubscribeEvent
+    public static void onCommonSetup(FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> TimeCore.INSTANCE.getCapabilityManager().attachStaticCoffeeCapability(CapabilityOwner.ENTITY, MY_CAPABILITY, entity -> entity instanceof Player, entity -> new MyPlayerCapability(((Player) entity))));
+    }
+}
