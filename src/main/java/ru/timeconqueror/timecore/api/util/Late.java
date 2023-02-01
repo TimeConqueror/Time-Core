5401b52f78d5452ae643278041eff99c067af403
TimeConqueror <timeconqueror999@gmail.com>
2023-02-01 18:54:55 +0300

    Backport: Add late-init holders

---
+++ src/main/java/ru/timeconqueror/timecore/api/util/Late.java
@@ -0,0 +1,182 @@
+package ru.timeconqueror.timecore.api.util;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public abstract class Late<T> implements Supplier<T> {
+    @Nullable
+    protected T value;
+
+    @Override
+    public T get() {
+        if (value == null) {
+            throw new IllegalStateException("Value hasn't been loaded yet.");
+        }
+
+        return value;
+    }
+
+    /**
+     * Returns {@code true} if there is a object present, otherwise {@code false}.
+     *
+     * @return {@code true} if there is a object present, otherwise {@code false}
+     */
+    public boolean isPresent() {
+        return this.value != null;
+    }
+
+    /**
+     * If a object is present, invoke the specified consumer with the object,
+     * otherwise do nothing.
+     *
+     * @param consumer block to be executed if a object is present
+     * @throws NullPointerException if object is present and {@code consumer} is
+     *                              null
+     */
+    public void ifPresent(Consumer<? super T> consumer) {
+        if (isPresent())
+            consumer.accept(get());
+    }
+
+    /**
+     * If a object is present, apply the provided mapping function to it,
+     * and if the result is non-null, return an {@code Optional} describing the
+     * result. Otherwise, return an empty {@code Optional}.
+     *
+     * @param <U>    The type of the result of the mapping function
+     * @param mapper a mapping function to apply to the object, if present
+     * @return an {@code Optional} describing the result of applying a mapping
+     * function to the object of this {@code Promised}, if a object is present,
+     * otherwise an empty {@code Optional}
+     * @throws NullPointerException if the mapping function is null
+     * @apiNote This method supports post-processing on optional values, without
+     * the need to explicitly check for a return status.
+     */
+    public <U> Optional<U> map(Function<? super T, ? extends U> mapper) {
+        Objects.requireNonNull(mapper);
+        if (!isPresent())
+            return Optional.empty();
+        else {
+            return Optional.ofNullable(mapper.apply(get()));
+        }
+    }
+
+    /**
+     * If a value is present, apply the provided {@code Optional}-bearing
+     * mapping function to it, return that result, otherwise return an empty
+     * {@code Optional}.  This method is similar to {@link #map(Function)},
+     * but the provided mapper is one whose result is already an {@code Optional},
+     * and if invoked, {@code flatMap} does not wrap it with an additional
+     * {@code Optional}.
+     *
+     * @param <U>    The type parameter to the {@code Optional} returned by
+     * @param mapper a mapping function to apply to the object, if present
+     *               the mapping function
+     * @return the result of applying an {@code Optional}-bearing mapping
+     * function to the value of this {@code Optional}, if a value is present,
+     * otherwise an empty {@code Optional}
+     * @throws NullPointerException if the mapping function is null or returns
+     *                              a null result
+     */
+    public <U> Optional<U> flatMap(Function<? super T, Optional<U>> mapper) {
+        Objects.requireNonNull(mapper);
+        if (!isPresent())
+            return Optional.empty();
+        else {
+            return Objects.requireNonNull(mapper.apply(get()));
+        }
+    }
+
+    /**
+     * If a object is present, lazily apply the provided mapping function to it,
+     * returning a supplier for the transformed result. If this object is empty, or the
+     * mapping function returns {@code null}, the supplier will return {@code null}.
+     *
+     * @param <U>    The type of the result of the mapping function
+     * @param mapper A mapping function to apply to the object, if present
+     * @return A {@code Supplier} lazily providing the result of applying a mapping
+     * function to the object of this {@code Promised}, if a object is present,
+     * otherwise a supplier returning {@code null}
+     * @throws NullPointerException if the mapping function is {@code null}
+     * @apiNote This method supports post-processing on optional values, without
+     * the need to explicitly check for a return status.
+     */
+    public <U> Supplier<U> lazyMap(Function<? super T, ? extends U> mapper) {
+        Objects.requireNonNull(mapper);
+        return () -> isPresent() ? mapper.apply(get()) : null;
+    }
+
+    /**
+     * Return the object if present, otherwise return {@code other}.
+     *
+     * @param other the object to be returned if there is no object present, may
+     *              be null
+     * @return the object, if present, otherwise {@code other}
+     */
+    public T orElse(T other) {
+        return isPresent() ? get() : other;
+    }
+
+    /**
+     * Return the object if present, otherwise invoke {@code other} and return
+     * the result of that invocation.
+     *
+     * @param other a {@code Supplier} whose result is returned if no object
+     *              is present
+     * @return the object if present otherwise the result of {@code other.get()}
+     * @throws NullPointerException if object is not present and {@code other} is
+     *                              null
+     */
+    public T orElseGet(Supplier<? extends T> other) {
+        return isPresent() ? get() : other.get();
+    }
+
+    /**
+     * Return the contained object, if present, otherwise throw an exception
+     * to be created by the provided supplier.
+     *
+     * @param <X>               Type of the exception to be thrown
+     * @param exceptionSupplier The supplier which will return the exception to
+     *                          be thrown
+     * @return the present object
+     * @throws X                    if there is no object present
+     * @throws NullPointerException if no object is present and
+     *                              {@code exceptionSupplier} is null
+     * @apiNote A method reference to the exception constructor with an empty
+     * argument list can be used as the supplier. For example,
+     * {@code IllegalStateException::new}
+     */
+    public <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier) throws X {
+        if (isPresent()) {
+            return get();
+        } else {
+            throw exceptionSupplier.get();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Late)) return false;
+
+        Late<?> late = (Late<?>) o;
+
+        return Objects.equals(value, late.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value != null ? value.hashCode() : 0;
+    }
+
+    public static class InsertableLate<T> extends Late<T> {
+        public void set(T value) {
+            this.value = value;
+        }
+    }
+}
