1ca8bf19ff80c9af01e189409c83ccdd976e4d9b
TimeConqueror <timeconqueror999@gmail.com>
2023-02-07 07:44:31 +0300

    Remove useless 1.19.3+ explode utils

--- src/main/java/ru/timeconqueror/timecore/api/util/LevelUtils.java
+++ src/main/java/ru/timeconqueror/timecore/api/util/LevelUtils.java
@@ -3,11 +3,7 @@ package ru.timeconqueror.timecore.api.util;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Explosion;
-import net.minecraft.world.level.ExplosionDamageCalculator;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
@@ -66,21 +62,5 @@ public class LevelUtils {
     @SuppressWarnings("unchecked")
     public static <E extends BlockEntity, A extends BlockEntity> BlockEntityTicker<A> makeSimpleTicker(BlockEntityType<A> providedType, Supplier<BlockEntityType<E>> requiredType, BlockEntityTicker<? super E> ticker) {
         return requiredType.get() == providedType ? (BlockEntityTicker<A>) ticker : null;
-    }
-
-    public static Explosion explode(Level level, @Nullable Entity entityIn, double xIn, double yIn, double zIn, float explosionRadius, Explosion.BlockInteraction modeIn) {
-        return explode(level, entityIn, null, null, xIn, yIn, zIn, explosionRadius, false, modeIn);
-    }
-
-    public static Explosion explode(Level level, @Nullable Entity entityIn, double xIn, double yIn, double zIn, float explosionRadius, boolean causesFire, Explosion.BlockInteraction modeIn) {
-        return explode(level, entityIn, null, null, xIn, yIn, zIn, explosionRadius, causesFire, modeIn);
-    }
-
-    public static Explosion explode(Level level, @Nullable Entity exploder, @Nullable DamageSource damageSource, @Nullable ExplosionDamageCalculator damageCalculator, double x, double y, double z, float size, boolean causesFire, Explosion.BlockInteraction mode) {
-        Explosion explosion = new Explosion(level, exploder, damageSource, damageCalculator, x, y, z, size, causesFire, mode);
-        if (net.minecraftforge.event.ForgeEventFactory.onExplosionStart(level, explosion)) return explosion;
-        explosion.explode();
-        explosion.finalizeExplosion(true);
-        return explosion;
     }
 }
