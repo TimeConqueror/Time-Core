32704f4d5808b866ccbfe41a65844925503192f0
TimeConqueror <timeconqueror999@gmail.com>
2023-02-08 04:11:19 +0300

    Backport: Add player coffee capability example 19.01.2023 20:58 | cherry-pick 1.19.3

--- src/test/java/ru/timeconqueror/timecore/capability_example/player/CEPCaps.java
+++ src/test/java/ru/timeconqueror/timecore/capability_example/player/CEPCaps.java
@@ -1,13 +1,15 @@
 package ru.timeconqueror.timecore.capability_example.player;
 
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import ru.timeconqueror.timecore.TimeCore;
 import ru.timeconqueror.timecore.api.registry.CapabilityRegister;
 import ru.timeconqueror.timecore.api.registry.util.AutoRegistrable;
+import ru.timeconqueror.timecore.api.util.Hacks;
 import ru.timeconqueror.timecore.common.capability.owner.CapabilityOwner;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -15,10 +17,16 @@ public class CEPCaps {
     @AutoRegistrable
     private static final CapabilityRegister REGISTER = new CapabilityRegister(TimeCore.MODID);
 
-    public static final Capability<MyPlayerCapability> MY_CAPABILITY = REGISTER.register(MyPlayerCapability.class);
+    @CapabilityInject(MyPlayerCapability.class)
+    public static final Capability<MyPlayerCapability> MY_CAPABILITY = Hacks.promise();
+
+    @AutoRegistrable.InitMethod
+    private static void register() {
+        REGISTER.regCapability(MyPlayerCapability.class);
+    }
 
     @SubscribeEvent
     public static void onCommonSetup(FMLCommonSetupEvent event) {
-        event.enqueueWork(() -> TimeCore.INSTANCE.getCapabilityManager().attachStaticCoffeeCapability(CapabilityOwner.ENTITY, MY_CAPABILITY, entity -> entity instanceof Player, entity -> new MyPlayerCapability(((Player) entity))));
+        event.enqueueWork(() -> TimeCore.INSTANCE.getCapabilityManager().attachStaticCoffeeCapability(CapabilityOwner.ENTITY, MY_CAPABILITY, entity -> entity instanceof PlayerEntity, entity -> new MyPlayerCapability(((PlayerEntity) entity))));
     }
 }
--- src/test/java/ru/timeconqueror/timecore/capability_example/player/MyPlayerCapability.java
+++ src/test/java/ru/timeconqueror/timecore/capability_example/player/MyPlayerCapability.java
@@ -1,45 +1,45 @@
 package ru.timeconqueror.timecore.capability_example.player;
 
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.network.PacketDistributor;
-import net.minecraftforge.network.simple.SimpleChannel;
+import net.minecraftforge.fml.network.PacketDistributor;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import ru.timeconqueror.timecore.common.capability.CoffeeCapabilityInstance;
+import ru.timeconqueror.timecore.common.capability.CoffeeCapability;
 import ru.timeconqueror.timecore.common.capability.owner.CapabilityOwner;
-import ru.timeconqueror.timecore.common.capability.owner.serializer.CapabilityOwnerCodec;
+import ru.timeconqueror.timecore.common.capability.owner.serializer.CapabilityOwnerSerializer;
 import ru.timeconqueror.timecore.common.capability.property.CoffeeProperty;
 import ru.timeconqueror.timecore.common.capability.property.serializer.IntPropertySerializer;
 
-public class MyPlayerCapability extends CoffeeCapabilityInstance<Entity> {
+public class MyPlayerCapability extends CoffeeCapability<Entity> {
     public final CoffeeProperty<Integer> ticks = prop("ticks", 0, IntPropertySerializer.INSTANCE).synced();
 
-    private final Player player;
+    private final PlayerEntity player;
 
-    public MyPlayerCapability(Player player) {
+    public MyPlayerCapability(PlayerEntity player) {
         this.player = player;
     }
 
     @NotNull
     @Override
-    public CapabilityOwnerCodec<Entity> getOwnerSerializer() {
+    public CapabilityOwnerSerializer<Entity> getOwnerSerializer() {
         return CapabilityOwner.ENTITY.getSerializer();
     }
 
     @NotNull
     @Override
-    public Capability<? extends CoffeeCapabilityInstance<Entity>> getCapability() {
+    public Capability<? extends CoffeeCapability<Entity>> getCapability() {
         return CEPCaps.MY_CAPABILITY;
     }
 
     @Override
     public void sendChangesToClients(@NotNull SimpleChannel channel, @NotNull Object data) {
-        if (player instanceof ServerPlayer serverPlayer) {
-            channel.send(PacketDistributor.PLAYER.with(() -> serverPlayer), data);
+        if (player instanceof ServerPlayerEntity) {
+            channel.send(PacketDistributor.PLAYER.with(() -> ((ServerPlayerEntity) player)), data);
         }
     }
 
@@ -52,7 +52,7 @@ public class MyPlayerCapability extends CoffeeCapabilityInstance<Entity> {
     }
 
     @Nullable
-    public static MyPlayerCapability of(Player player) {
+    public static MyPlayerCapability of(PlayerEntity player) {
         LazyOptional<MyPlayerCapability> cap = player.getCapability(CEPCaps.MY_CAPABILITY);
         if (cap.isPresent()) {
             return cap.orElseThrow(IllegalStateException::new);
